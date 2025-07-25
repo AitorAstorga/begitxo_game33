8ae5d035e5b927f3190f6a7d8237a276ad1bd04d
Aitor <a.astorga.sdv@protonmail.com>
2025-07-25 12:18:51 +0200

    Add shock assets

--- src/assets.rs
+++ src/assets.rs
@@ -21,18 +21,19 @@ pub const SCENE_1_IRRITATED_2: &str = "./img/begitxo_irritated2.png";
 pub const SCENE_1_IRRITATED_3: &str = "./img/begitxo_irritated3.png";
 pub const SCENE_1_IRRITATED_4: &str = "./img/begitxo_irritated4.png";
 
+pub const SCENE_1_SHOCK_1: &str = "./img/begitxo_shock1.png";
+pub const SCENE_1_SHOCK_2: &str = "./img/begitxo_shock2.png";
+
 pub const SCENE_2_SUN: &str = "./img/begitxo_sun.png";
 
-
-pub const SCENE_3: &str = "./img/test_escena3.png";
 
 // ==========================
 // Sounds
 // ==========================
 
 pub const SCENE_1_MUSIC: &str = "./sound/scene1.ogg";
-pub const SCENE_1_AMBIENT: &str = "./sound/scene1_ambient.mp3";
-pub const SCENE_1_SHOCK: &str = "./sound/scene1_electric_shock.ogg";
+pub const SCENE_1_AMBIENT: &str = "./sound/scene1.ogg"; // TO DO: ERROR WITH MP3 IN SOME SYSTEMS BECAUSE YES "./sound/scene1_ambient.mp3";
+pub const SCENE_1_SHOCK_SOUND: &str = "./sound/scene1_electric_shock.ogg";
 
 // ==========================
 // Menu and Other UI Images
