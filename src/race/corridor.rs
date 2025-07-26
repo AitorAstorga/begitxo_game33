36bc13642d89061ced8e79032fb0fe6041b58524
Thadah D. Denyse <thadahdenyse@protonmail.com>
2025-07-26 13:31:40 +0200

    Fix corridor not stretching far enough

--- src/race/corridor.rs
+++ src/race/corridor.rs
@@ -20,18 +20,42 @@ impl CorridorScroller {
         Self { w: tex.width(), tex, x: 0.0, speed, dir, loops_goal: tiles, loops_done: 0 }
     }
 
-    #[inline] pub fn update(&mut self) {
+    #[inline]
+    pub fn update(&mut self) {
         let dt = get_frame_time();
-        self.x += match self.dir { ScrollDir::Left => -self.speed, ScrollDir::Right => self.speed } * dt;
-        if self.x <= -self.w { self.x += self.w; self.loops_done += 1; }
-        else if self.x >=  self.w { self.x -= self.w; self.loops_done += 1; }
+        self.x += match self.dir {
+            ScrollDir::Left => -self.speed,
+            ScrollDir::Right => self.speed,
+        } * dt;
+        if self.x <= -self.w {
+            self.x += self.w;
+            self.loops_done += 1;
+        } else if self.x >= self.w {
+            self.x -= self.w;
+            self.loops_done += 1;
+        }
     }
 
-    #[inline] pub fn finished(&self) -> bool { self.loops_done >= self.loops_goal }
+    #[inline]
+    pub fn finished(&self) -> bool {
+        self.loops_done >= self.loops_goal
+    }
 
-    #[inline] pub fn draw(&self) {
-        draw_texture(&self.tex, self.x, 0.0, WHITE);
-        let other_x = if self.dir == ScrollDir::Left { self.x + self.w } else { self.x - self.w };
-        draw_texture(&self.tex, other_x, 0.0, WHITE);
+    #[inline]
+    pub fn draw(&self) {
+        let full_h = screen_height();
+        let params = DrawTextureParams {
+            dest_size: Some(vec2(self.w, full_h)),
+            .. Default::default()
+        };
+
+        // draw the two tiles, each stretched to full_h
+        draw_texture_ex(&self.tex, self.x,       0.0, WHITE, params.clone());
+        let other_x = if self.dir == ScrollDir::Left {
+            self.x + self.w
+        } else {
+            self.x - self.w
+        };
+        draw_texture_ex(&self.tex, other_x, 0.0, WHITE, params);
     }
 }
